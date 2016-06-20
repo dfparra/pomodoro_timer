83b13977f259c08a565185d6e709eb922ff40a13
Daniel <danielparra93@gmail.com>
2016-06-20 19:56:08 -0400

    created seconds countdown

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,24 @@
-console.log('My JavaScript is working...!!!!');
+
+$(document).ready(function(){
+  var start = $('#start');
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+  // console.log(start);;
+  // console.log(minutes);
+  // console.log(seconds);
+  start.on('click',startCountdown);
+
+  function startCountdown(){
+    setInterval(function(){
+      var secondsVal = +seconds.text();//the plus sign makes this behave like a number
+      if(secondsVal === 0){
+        seconds.text(59);
+      } else{
+        seconds.text(secondsVal - 1);
+      }
+
+      // console.log(typeof +secondsVal);
+    }, 1000)
+  }
+
+});
